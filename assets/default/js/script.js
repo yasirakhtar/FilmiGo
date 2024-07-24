7dcbbaf25e39e6ad1f5580a1f0fb264f3bc58328
Yasir Akhtar <yasiralam662@gmail.com>
2024-07-24 23:54:14 +0530

    Page Ready !

--- assets/default/js/script.js
+++ assets/default/js/script.js
@@ -17,3 +17,35 @@ function openSearchPopUpBox() {
 function closeSearchPopUpBox() {
     searchPopupBoxMain.style.display = "none";
 }
+
+// search Js
+
+let searchUrl = window.location.search.slice(3)
+        let searchText = searchUrl.split("+").join(" ")
+    
+        if (searchText == "") {
+            document.getElementById("title").innerHTML = `All Movies`
+        } else {
+            document.getElementById("title").innerHTML = `Search Results for ➤ <b>${searchText}</b>`
+        }
+    
+        function searchMovie() {
+            let input = searchText
+            input = input.toLowerCase();
+            let allMovies = document.getElementById("allMovies");
+            let x = document.getElementsByClassName('movie');
+    
+            for (i = 0; i < x.length; i++) {
+                if (!x[i].innerHTML.toLowerCase().includes(input)) {
+                    x[i].style.display = "none";
+                    allMovies.style.justifyContent = "left";
+                    allMovies.style.gap = "15px";
+                }
+                else {
+                    x[i].style.display = "block";
+                    document.getElementById('notFound').style.display = "none";
+                }
+            }
+        }
+
+        searchMovie();
